856fdb3f7e62ef8a5089ef27562dcc0033f18c7b
MGT <mgtrolls@hotmail.co.uk>
2021-09-09 10:33:39 +0100

    Updated instructions and files
    Updated instructions and files

--- EXAMPLE_Config.cpp
+++ EXAMPLE_Config.cpp
@@ -1,6 +1,56 @@
-class ClaimVehicle: ExileAbstractAction
+class Tank
 {
-    title = "Claim Ownership";
-    condition = "true";
-    action = "call ExileClient_ClaimVehicles_network_claimRequestSend";
+    targetType = 2;
+    target = "Tank";
+    class Actions 
+    {
+        class ClaimVehicle: ExileAbstractAction
+        {
+            title = "Claim Ownership";
+            condition = "true";
+            action = "call ExileClient_ClaimVehicles_network_claimRequestSend";
+        };
+    };
+};
+class Car
+{
+    targetType = 2;
+    target = "Car";
+    class Actions 
+    {
+        class ClaimVehicle: ExileAbstractAction
+        {
+            title = "Claim Ownership";
+            condition = "true";
+            action = "call ExileClient_ClaimVehicles_network_claimRequestSend";
+        };
+    };
+};
+class Air
+{
+    targetType = 2;
+    target = "Air";
+    class Actions 
+    {
+        class ClaimVehicle: ExileAbstractAction
+        {
+            title = "Claim Ownership";
+            condition = "true";
+            action = "call ExileClient_ClaimVehicles_network_claimRequestSend";
+        };
+    };
+};
+class Boat
+{
+    targetType = 2;
+    target = "Boat";
+    class Actions 
+    {
+        class ClaimVehicle: ExileAbstractAction
+        {
+            title = "Claim Ownership";
+            condition = "true";
+            action = "call ExileClient_ClaimVehicles_network_claimRequestSend";
+        };
+    };
 };
--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # Claim-Vehicles
 Allows players to save non-persistent vehicles using a code lock (Exile Mod)
 
-Ask in Exile Discord how to install http://www.exilemod.com/discord
+Instructions
+
+Drop the ClaimVehicles_Server.pbo into your @ExileServer\addons folder
+
+Unpack your mission file
+
+Drop the ClaimVehicles_Client folder into your mission file
+
+Add the contents of initPlayerLocal.sqf into the file of the same name in your mission file,
+if it doesn't exist, create it
+
+Copy the contents of EXAMPLE_Config.cpp into your mission config in class CfgInteractionMenus
+in each respective section for Tank, Car, Air and Boat
+
+Pack your mission file
+
+
+Ask in Exile Discord how to install if you get stuck http://www.exilemod.com/discord
